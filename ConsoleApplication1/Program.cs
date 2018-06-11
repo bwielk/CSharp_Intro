2d37635ba6d70a69bc926ebdbcdf05637f49bd1c
bwielk <bwielk@gmail.com>
2018-06-11 21:30:38 +0100

    Intro to C#

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, it's my first code in C#");
+            Console.ReadLine();
+            Console.Write("and here's more text following the command");
+            Console.ReadLine();
+        }
+    }
+}
